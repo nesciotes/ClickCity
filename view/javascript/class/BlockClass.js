5bae1d65754d51b054ba85b66da133fa5c5a15ac
mjjh <martijnvriens94@gmail.com>
2014-06-08 02:01:57 +0200

    test

---
+++ view/javascript/class/BlockClass.js
@@ -0,0 +1,14 @@
+/**
+ * BlockClass
+ */
+
+function Block($_PARAMETER) {
+	var pos;  	// Position
+	var type; 	// Blocktype
+	var light;	// int
+	
+	var constructor = function(){
+		pos = $_PARAMETER['pos'];
+		type = $_PARAMETER['type'];
+	}
+}
