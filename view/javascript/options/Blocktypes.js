7a29daee4922d613f04e6fe869c21fd0ec239779
mjjh <martijnvriens94@gmail.com>
2014-06-18 15:21:19 +0200

    options toegevoegd - om dingen aanpassen op één plek te houden

---
+++ view/javascript/options/Blocktypes.js
@@ -0,0 +1,19 @@
+var BlockTypes = {
+	Grass: new Blocktype(
+			{
+				top: 
+				[
+				 	{texture: "grass.png"}
+				],
+				side: 
+				[
+				 	{texture: "dirt.png"}
+				]
+			},
+			{
+				width: 1,
+				height: 1,
+				lengt: 1
+			},
+			true),
+};
---
+++ view/javascript/options/Statics.js
@@ -0,0 +1,4 @@
+var STATICS = {
+	clockspeed: 	6,
+	movespeed:		10,
+};
