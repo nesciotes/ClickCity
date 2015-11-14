c15d100b8e108a6b4295b838d6e4c0f94e8d75d5
NesciO <mayflair@live.nl>
2015-11-14 08:29:52 +0100

    Update and rename AnimateEmrakul.js to PointClass.js

---
+++ view/javascript/class/PointClass.js
@@ -0,0 +1,13 @@
+ function Point(_x, _y, _z)
+{
+	this.x = _x;	// float
+	this.y = _y;	// float
+	this.z = _z;	// float
+};
+
+Point.prototype.distanceTo = function(pointTo)
+{
+	return Math.sqrt(Math.pow(pointTo.x - this.x, 2) + 
+					 Math.pow(pointTo.y - this.y, 2) +
+					 Math.pow(pointTo.z - this.z, 2));
+}; 
